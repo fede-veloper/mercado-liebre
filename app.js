2904a7c6fd2697442543c9eea33d7964b2990b81
Federico Villegas <federico@iMac-de-Federico.local>
2021-06-24 09:04:51 -0500

    version-1

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+const PORT = 3000;
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.get('/views/index.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.get('/views/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+// npm install --save-dev nodemon
+// npx nodemon app.js
