66cb5cec22945608a7d80d1d278c1d48794a02cd
Federico Villegas <federico@iMac-de-Federico.local>
2021-07-01 17:37:03 -0500

    add-/-app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
 app.get('/views/index.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
