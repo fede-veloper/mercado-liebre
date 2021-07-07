d8e50eb509475cf0897154865f9fc930a9b8b779
fede-veloper <fedev.code@gmail.com>
2021-07-07 08:13:46 -0500

    quit-routes

--- app.js
+++ app.js
@@ -6,8 +6,24 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.use('/src/views', rutasMain);
-app.use('/', rutasMain);
+//app.use('/src/views', rutasMain);
+//app.use('/', rutasMain);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'src/views/index.html'));
+});
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'src/views/index.html'));
+}); 
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'src/views/register.html'));
+});
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'src/views/login.html'));
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
